15bb06578bcfb48572f65b4c88bbedac98c5c026
Hayri Can GÜNDOĞDU <hc_gundogdu@aof.anadolu.edu.tr>
2021-01-24 00:57:19 +0300

    insert

---
+++ intro/src/main/java/com/abc/intro/App.java
@@ -0,0 +1,16 @@
+package com.abc.intro;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
+
+
+//src.main.java/com.abc.intro/App.java Bakın burası bildiğimiz java projesi ama tek bir farkla MAVEN yönetimi var burada arkadaşlar!
